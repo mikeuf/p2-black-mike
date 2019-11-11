d9974184481f8e3dc99fb9ffa90f083ec0c1cffe
Mike <mblack101@ufl.edu>
2019-11-11 00:17:32 -0500

    Narrow main content to 960px, and replace background image with edited version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
  window.onload = function() {
 headerTag = document.getElementsByTagName('header');
-headerTag[0].innerHTML = '<a class="logo" href="index.html"> <i class="fal fa-landmark"></i> <h1>Debary Public Library Association</h1> </a> <nav> <ul> <li><a href="about.html"><i class="fal fa-book-user"></i>About</a></li><li><a href="contact.html"><i class="fal fa-phone-rotary"></i>Contact</a></li></ul> </nav>';
+headerTag[0].innerHTML = '<a class="logo" href="index.html"> <i class="fal fa-landmark"></i> <h1>Debary Public <br />Library Association</h1> </a> <nav> <ul> <li><a href="about.html"><i class="fal fa-book-user"></i><p>About</p></a></li><li><a href="contact.html"><i class="fal fa-phone-rotary"></i><p>Contact</p></a></li></ul> </nav>';
 
 footerTag = document.getElementsByTagName('footer');
 footerTag[0].innerHTML = '<p> &copy; 2019 Debary Public Library Association, Inc. All rights reserved. </p>';
