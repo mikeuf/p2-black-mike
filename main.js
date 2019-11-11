37b42bc7ab494ebd89820fc73f46e1bd0dbac9bd
Mike <mblack101@ufl.edu>
2019-11-10 23:11:27 -0500

    Relocated header and footer to javascript to avoid duplicating code

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 
+ window.onload = function() {
+headerTag = document.getElementsByTagName('header');
+headerTag[0].innerHTML = '<a class="logo" href="#"> <i class="fad fa-landmark"></i> <h1>Debary Public Library Association</h1> </a> <nav> <ul> <li> About </li><li> <i class="fal fa-grip-lines-vertical"></i> </li><li> Location </li><li> <i class="fal fa-grip-lines-vertical"></i> </li><li> Events </li><li> <i class="fal fa-grip-lines-vertical"></i> </li><li> Contact </li></ul> </nav>';
+
+footerTag = document.getElementsByTagName('footer');
+footerTag[0].innerHTML = '<p> &copy;2019 Debary Public Library Association, Inc. All rights reserved. </p>';
+
+ };
