07bea1bb1988285137f8deb82a88767806693b78
Mike <mblack101@ufl.edu>
2019-11-10 23:27:31 -0500

    Adjust and stylize nav links

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
  window.onload = function() {
 headerTag = document.getElementsByTagName('header');
-headerTag[0].innerHTML = '<a class="logo" href="#"> <i class="fad fa-landmark"></i> <h1>Debary Public Library Association</h1> </a> <nav> <ul> <li> About </li><li> <i class="fal fa-grip-lines-vertical"></i> </li><li> Location </li><li> <i class="fal fa-grip-lines-vertical"></i> </li><li> Events </li><li> <i class="fal fa-grip-lines-vertical"></i> </li><li> Contact </li></ul> </nav>';
+headerTag[0].innerHTML = '<a class="logo" href="index.html"> <i class="fal fa-landmark"></i> <h1>Debary Public Library Association</h1> </a> <nav> <ul> <li><a href="about.html"><i class="fal fa-book-user"></i>About</a></li><li><a href="contact.html"><i class="fal fa-phone-rotary"></i>Contact</a></li></ul> </nav>';
 
 footerTag = document.getElementsByTagName('footer');
-footerTag[0].innerHTML = '<p> &copy;2019 Debary Public Library Association, Inc. All rights reserved. </p>';
+footerTag[0].innerHTML = '<p> &copy; 2019 Debary Public Library Association, Inc. All rights reserved. </p>';
 
  };
