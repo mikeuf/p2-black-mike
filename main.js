4155a7918eabd73e31eb6b1c7c2fcdf2b7b18218
Mike <mblack101@ufl.edu>
2019-11-13 22:52:38 -0500

    Add hamburger menu button

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 window.addEventListener('load', (event) => {
 
     headerTag = document.getElementsByTagName('header');
-    headerTag[0].innerHTML = '<a class="logo" href="index.html"> <i class="fal fa-landmark"></i> <h1>Debary Public <br />Library Association</h1> </a> <nav> <ul> <li><a href="about.html"><i class="fal fa-book-user"></i><p>About</p></a></li><li><a href="contact.html"><i class="fal fa-phone-rotary"></i><p>Contact</p></a></li></ul> </nav>';
+    headerTag[0].innerHTML = '<a class="logo" href="index.html"> <i class="fal fa-landmark"></i> <h1>Debary Public <br />Library Association</h1> </a> <div class="hamburger-menu"><i class="fal fa-bars"></i></div> <nav> <ul> <li><a href="about.html"><i class="fal fa-book-user"></i><p>About</p></a></li><li><a href="contact.html"><i class="fal fa-phone-rotary"></i><p>Contact</p></a></li></ul> </nav>';
 
     footerTag = document.getElementsByTagName('footer');
     footerTag[0].innerHTML = '<p> &copy; 2019 Debary Public Library Association, Inc. All rights reserved. </p>';
